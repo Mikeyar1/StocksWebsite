286f3f57382c03dd1cbd4dbd49f9fc355b39de11
Ubuntu <avilaoe@miamioh.edu>
2024-05-01 20:28:55 +0000

    started writng api call to get stocks

--- scripts.js
+++ scripts.js
@@ -1,47 +1,32 @@
+var exchanges; 
 
 $(document).ready(function() {
     getExchanges();
 
     $("#exchangeSelect").on('change', function() {
-        getStocks(this.value);
+        alert($("#exchangeSelect").prop('selectedIndex'));
+        //getStocks()
     });
 });
-
-// function getExchanges() {
-//     a = $.ajax({
-//         url: "https:api.polygon.io/v1/meta/exchanges/",
-//         method: "GET",
-//         dataType: 'jsonp',
-//         data: {
-//           apiKey: "Et0sBzqfH5pR4lpBy2wWp1_PPeo6OMK2"
-//         }
-//     }).done(function(data) {
-//         for(let i = 0; i < data.length; i++) {
-//             $("#exchangeSelect").append(`<option>${data[i].name}</option>`)
-//         }
-//     }).fail(function(error) {
-//         console.log("error", error.statusText);
-//     })
-// }
 
 function getExchanges() {
     a = $.ajax({
         url: "https://api.polygon.io/v3/reference/exchanges?asset_class=stocks&apiKey=Et0sBzqfH5pR4lpBy2wWp1_PPeo6OMK2",
         method: "GET"
     }).done(function(data) {
-        for(let i = 0; i < data.results.length; i++) {
-            $("#exchangeSelect").append(`<option>${data.results[i].name}</option>`)
-            console.log(data.results[i].name);
+        exchanges = data;
+        for(let i = 0; i < exchanges.results.length; i++) {
+            $("#exchangeSelect").append(`<option>${exchanges.results[i].name}</option>`)
         }
     }).fail(function(error) {   
         console.log("error", error.statusText);
     })
 }
 
-function getStocks(stockName) {
+function getStocks(index) {
     a = $.ajax({
-        //url: url
-        // method: "GET"
+        url: "https://api.polygon.io/v3/reference/tickers?exchange=XNYS&active=true&apiKey=Et0sBzqfH5pR4lpBy2wWp1_PPeo6OMK2",
+        method: "GET"
     }).done(function(data) {
         // fill stockSelect
     }).fail(function(error) {
