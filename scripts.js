52d2f6bcf85e3c3c82e2d4abe477198f59b017c9
Oscar Avila <avilaoe@miamioh.edu>
2024-05-06 19:32:03 +0000

    work on login system

--- scripts.js
+++ scripts.js
@@ -4,10 +4,6 @@ $(document).ready(function () {
         $("#loginModal").modal("toggle");
         $("#signUpModal").modal("toggle");
     });
-
-    $("#signUpButton").on("submit", function() {
-        alert("submit");
-    });
 });
 
 function signUp() {
@@ -15,11 +11,44 @@ function signUp() {
     username = $("#usernameSignUp").val();
     pw = $("passwordSignUp").val();
 
+    myName = myName.trim();
+    myName = myName.replace(/ /g, '+');
+
     a = $.ajax({
         url: `http://172.17.12.211/cse383_final/final.php/signUp?name=${myName}&username=${username}&password=${pw}`
     }).done(function(data) {
-
-    }).fail(function(data) {
-
+        $("#signUpErrorMessage").text("");
+        if(data.status == 0) {
+            $("#signUpModal").modal("toggle");
+            $("#loginModal").modal("toggle");
+            $("#loginGoodMessage").text("");
+            $("#loginGoodMessage").text("Successfully signed up!");
+        } else {
+            $("#signUpErrorMessage").text("");
+            $("#signUpErrorMessage").text(data.message);
+        }
+    }).fail(function(error) {
+        console.log("error", error.statusText);
     });
+}
+
+function login() {
+    username = $("#usernameLogin").val();
+    pw = $("#passwordLogin").val();
+
+    a = $.ajax({
+        url: `http://172.17.12.211/cse383_final/final.php/login?username=${username}&password=${pw}`
+    }).done(function(data) {
+        $("#loginErrorMessage").text("");
+        if(data.status == 0) {
+            $("#loginModal").modal("toggle");
+            $("#loginButton").toggle
+            
+        } else {
+            $("#loginErrorMessage").text("");
+            $("#loginErrorMessage").text(data.message);
+        }
+    }).fail(function(error) {
+        console.log("error", error.statusText);
+    })
 }
