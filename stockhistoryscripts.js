54d3854fc932e77c7277b6ae381cc10d762b99e2
ariasm <ariasm@miamioh.edu>
2024-05-08 12:36:37 -0400

    Update stockhistoryscripts.js.    I added a drawChart and getStockTradingData functions which intends to create a chart with the stock prices.

--- stockhistoryscripts.js
+++ stockhistoryscripts.js
@@ -21,7 +21,7 @@ function getHistory() {
     date2 = $("#dateEnd").val();
     sort = $("#sortSelect").val();
     a = $.ajax({
-        url: `http://172.17.12.211/cse383_final/final.php/getHistory?username=${username}&date1=${date1}&date2=${date2}&sort=${sort}`,
+        url: `http://172.17.12.211/cse383_final/final.php/getHistory?{username}=&date1=${date1}&date2=${date2}&sort=${sort}`,
         method: "GET"
     }).done(function(data) {
         if(data.data.length > 0) {
@@ -66,4 +66,45 @@ function getFavorites() {
     }).fail(function(error){
         console.log("error", error.statusText);
     })
+}
+
+function getStockTradingData(ticker) {
+    $.ajax({
+        url: '',
+        method: "GET"
+    }).done(function(data) {
+        drawChart(data);
+    }).fail(function(error) {
+        console.log("Error fetching trading data:", error.statusText);
+    });
+}
+function drawChart(stockData) {
+    var ctx = $('#stockChart').get(0).getContext('2d');  
+
+    var chartLabels = stockData.map(function(entry) {
+        return new Date(entry.t).toLocaleDateString();
+    });
+    var chartData = stockData.map(function(entry) {
+        return entry.c;
+    });
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: chartLabels,
+            datasets: [{
+                label: 'Closing Price',
+                data: chartData,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
+            }
+        }
+    });
 }
