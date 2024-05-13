96e6c2dc07f873ddfd0ac260c67a91ef1f010db1
Oscar Avila <avilaoe@miamioh.edu>
2024-05-13 22:56:24 +0000

    finished favorites page

--- stockhistoryscripts.js
+++ stockhistoryscripts.js
@@ -1,34 +1,42 @@
+var rowCounter = 0;
 
-$(document).ready(function() {
+$(document).ready(function () {
     $("#loginModal").modal("toggle");
 
-    $("#historyForm").on("submit", function(event) {
+    $("#historyForm").on("submit", function (event) {
         event.preventDefault();
         getHistory();
     });
 
-    $("#sortSelect").on("change", function() {
+    $("#endOfDayForm").on("submit", function (event) {
+        event.preventDefault();
+        calculateFavorites();
+    });
+
+    $("#sortSelect").on("change", function () {
         getHistory();
     });
 
-    $("#loginModal").on("hide.bs.modal", function() {
+    $("#loginModal").on("hide.bs.modal", function () {
         getFavorites();
+        //getFavoritesSummary();
+
     })
 
-    $('#favoritesTable').on('click', 'button', function() {
+    $('#manageTable').on('click', 'button', function () {
         var ticker = $(this).closest('tr').find('td:eq(2)').text();
-        
+
         a = $.ajax({
             url: `http://172.17.12.211/cse383_final/final.php/removeFavorite?username=${username}&ticker=${ticker}`,
             method: "GET"
-        }).done(function(data) {
+        }).done(function (data) {
             getFavorites();
             console.log(data.data.stock);
             logFavoritesAction(data.data[0].stock, ticker, "removed");
-        }).fail(function(error) {
+        }).fail(function (error) {
             console.log("error", error.statusText);
         });
-      });
+    });
 });
 
 function getHistory() {
@@ -38,69 +46,79 @@ function getHistory() {
     a = $.ajax({
         url: `http://172.17.12.211/cse383_final/final.php/getHistory?username=${username}&date1=${date1}&date2=${date2}&sort=${sort}`,
         method: "GET"
-    }).done(function(data) {
-        if(data.data.length > 0) {
-            $("#historyTable").html("");
-            for(let i = 0; i < data.data.length; i++) {
-                $("#historyTable").append(`
+    }).done(function (data) {
+        $("#historyTable").html("");
+        for (let i = 0; i < data.data.length; i++) {
+            $("#historyTable").append(`
                 <tr>
                 <td>${data.data[i].date}</td>
                 <td>${data.data[i].stock}</td>
                 <td>${data.data[i].ticker}</td>
                 <td>${data.data[i].action}</td>
                 </tr>`)
-            }
-        } else {
-            //no history found
         }
-    }).fail(function(error) {
+
+    }).fail(function (error) {
         console.log("error", error.statusText);
     })
 }
 
 function getFavorites() {
     username = $("#usernameLogin").val();
-    
+
     a = $.ajax({
         url: `http://172.17.12.211/cse383_final/final.php/getFavorites?username=${username}`,
         method: "GET"
-    }).done(function(data) {
-        if(data.data.length > 0) {
-            $("#favoritesTable").html("");
-            for(let i = 0; i < data.data.length; i++) {
-                $("#favoritesTable").append(`
+    }).done(function (data) {
+        $("#manageTable").html("");
+        for (let i = 0; i < data.data.length; i++) {
+            $("#manageTable").append(`
                 <tr>
                 <td>${data.data[i].date}</td>
                 <td>${data.data[i].stock}</td>
                 <td>${data.data[i].ticker}</td>
                 <td><button type="button" class="btn btn-danger">Remove</button></td>
                 </tr>`)
-            }
-        } else {
-            // no history found
         }
-    }).fail(function(error){
+    }).fail(function (error) {
         console.log("error", error.statusText);
     })
 }
 
-function getStockTradingData(ticker) {
+function getStockTradingData(ticker, name) {
+    date = getDate();
     $.ajax({
-        url: '',
+        url: `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${date}/${date}?adjusted=true&sort=asc&apiKey=Et0sBzqfH5pR4lpBy2wWp1_PPeo6OMK2`,
         method: "GET"
-    }).done(function(data) {
-        drawChart(data);
-    }).fail(function(error) {
+    }).done(function (data) {
+        $("#favoritesTable").append(`<tr></tr>`);
+        $("#favoritesTable").append(`<td>${ticker}</td>`);
+        $("#favoritesTable").append(`<td>${name}</td>`);
+        $("#favoritesTable").append(`<td>${data.results[0].o}</td>`);
+        $("#favoritesTable").append(`<td>${data.results[0].h}</td>`);
+        $("#favoritesTable").append(`<td>${data.results[0].l}</td>`);
+        $("#favoritesTable").append(`<td>${data.results[0].c}</td>`);
+
+        percent = ((data.results[0].l - data.results[0].c) / (data.results[0].c) * 100).toFixed(2);
+        if (percent > 0) {
+            $("#favoritesTable").append(`<td class="positive">↑${percent}</td>`);
+        } else {
+            $("#favoritesTable").append(`<td class="negative">↓${percent}</td>`);
+        }
+
+        $("#favoritesTable").append(`<td>${data.results[0].v}</td>`);
+
+    }).fail(function (error) {
         console.log("Error fetching trading data:", error.statusText);
     });
 }
 function drawChart(stockData) {
-    var ctx = $('#stockChart').get(0).getContext('2d');  
+    var ctx = $('#stockChart').get(0).getContext('2d');
 
-    var chartLabels = stockData.map(function(entry) {
+    var chartLabels = stockData.map(function (entry) {
         return new Date(entry.t).toLocaleDateString();
     });
-    var chartData = stockData.map(function(entry) {
+    var chartData = stockData.map(function (entry) {
         return entry.c;
     });
 
@@ -123,4 +141,70 @@ function drawChart(stockData) {
             }
         }
     });
+}
+
+function getDate() {
+    date = new Date();
+    year = date.getFullYear();
+    month = date.getMonth();
+    day = date.getDay();
+
+    if (month < 10) {
+        month = "0" + month
+    }
+
+    if (day < 10) {
+        day = "0" + day
+    }
+
+    return `${year}-${month}-${day}`
+}
+
+function getFavoritesSummary() {
+    a = $.ajax({
+        url: `http://172.17.12.211/cse383_final/final.php/getFavorites?username=${username}`,
+        method: "GET"
+    }).done(function (data) {
+        console.log(username)
+        for (let i = 0; i < data.data.length; i++) {
+            getStockTradingData(data.data[i].ticker, data.data[i].stock);
+        }
+    }).fail(function (error) {
+        console.log("error", error.statusText);
+    })
+}
+
+function calculateFavorites() {
+    date = $("#endOfDay").val();
+    history;
+    a = $.ajax({
+        url: `http://172.17.12.211/cse383_final/final.php/getHistory?username=${username}&date1=1753-01-01&date2=${date}&sort=asc`,
+        method: "GET"
+    }).done(function (data) {
+        myHistory = new Set();
+        indexes = [];
+        for(let i = 0; i < data.data.length; i++) {
+            if(myHistory.has(data.data[i].ticker)) {
+                myHistory.delete(data.data[i].ticker);
+                indexes.splice(i, 1);
+            } else {
+                myHistory.add(data.data[i].ticker);
+                indexes.push(i);
+            }
+        }
+
+        const interator = myHistory.entries();
+        $("#endOfDayTable").html("");
+        for (let i = 0; i < myHistory.size; i++) {
+            $("#endOfDayTable").append(`
+                <tr>
+                <td>${data.data[indexes[i]].date}</td>
+                <td>${data.data[indexes[i]].stock}</td>
+                <td>${data.data[indexes[i]].ticker}</td>
+                </tr>`)
+        }
+
+    }).fail(function (error) {
+        console.log("error", error.statusText);
+    }) 
 }
